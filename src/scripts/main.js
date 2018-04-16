6c17cd1fe42b57f6345d60ed1d9f4e8b8ba21ac0
Phạm Đỗ Hưng <phamdohung161@gmail.com>
2018-04-16 17:31:02 +0700

    Add eslint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,25 @@
-window.AwesomeScript = (function() {
-  var _this = {};
-
-  _this.init = function() {
+window.vBaseScript = (() => {
+  const utils = {
+    isMobile: agent => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent || window.navigator.userAgent),
+    fixIE: () => {
+      if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        const msViewportStyle = document.createElement('style');
+        msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
+        document.querySelector('head').appendChild(msViewportStyle);
+      }
+    },
   };
 
-  _this.utils = {
-    isMobile: function(agent) {
-      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent || window.navigator.userAgent);
-    }
+  const init = () => {
+    /* Fix IE */
+    utils.fixIE();
   };
 
-  _this.fixIE = function() {
-    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-      var msViewportStyle = document.createElement('style')
-      msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'))
-      document.querySelector('head').appendChild(msViewportStyle)
-    }
+  return {
+    init,
   };
-
-  return _this;
 })();
 
-$('document').ready(function() {
-  window.AwesomeScript.init();
+$('document').ready(() => {
+  window.vBaseScript.init();
 });
